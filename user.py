195d266e25904124c024658e2411f47592fccf55
Maximus Young <maxyoung0320@gmail.com>
2023-11-09 09:50:35 -0600

    start user class

---
+++ user.py
@@ -0,0 +1,13 @@
+class User():
+    def __init__(self):
+        self.databaseName = ""
+        self.tableName = ""
+        self.loggedIn = False
+        self.userID = ""
+    def __init__(self, databaseName, tableName):
+        self.databaseName = ""
+        self.tableName = ""
+        self.loggedIn = False
+        self.userID = ""
+    def login(self):
+        userID = 6
