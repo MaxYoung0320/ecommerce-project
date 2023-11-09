21eec8cbe911c5c6ee6b22bd25fdf8f03ac7745b
cruxsavi <92225430+cruxsavi@users.noreply.github.com>
2023-11-09 10:01:15 -0600

    Update cart.py

---
+++ cart.py
@@ -0,0 +1,19 @@
+class Cart():
+    def __init__(self, databaseName, tableName):
+        self.databaseName = databaseName
+        self.tableName = tableName
+
+    def __init__(self):
+        pass
+
+    def viewCart(self, userID, inventoryDatabase):
+        pass
+
+    def addToCart(self, userID, ISBN):
+        pass
+
+    def removeFromCart(self, userID, ISBN):
+        pass
+
+    def checkOut(self, userID):
+        pass
