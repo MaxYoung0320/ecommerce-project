b289e05dbcacb1d3417be3786431665aa1285b8d
amanuel tesfaye <amanuelttd17@gmail.com>
2023-11-09 09:30:28 -0600

    Add nums.cpp file

---
+++ nums.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int num = 5;
+    double dnum = 5.5;
+
+    dnum += num;
+    cout << dnum <<endl;
+    // 10/3 return 3
+    cout << -40<< endl;
+    // operation same as python
+    cout << 5 + 7 <<endl;
+
+    cout << pow(2, 5)<< endl;
+    cout << round(4.8) << endl;
+    cout << fmax(5,2) << fmin(5,2)<<endl;
+
+    return 0;
+}
