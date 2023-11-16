5fe7ee7320a97bd6c2847303050f889916d21ab9
Johnnybro11 <landonjcessna@gmail.com>
2023-11-16 11:11:06 -0600

    testing view

--- inv.py
+++ inv.py
@@ -1,4 +1,24 @@
+import sqlite3
+import sys
+
 class inv():
+    #opening the db
+    try:
+    connection = sqlite3.connect("methods.db")
+
+    print("Successful connection.")
+
+except:
+    print("Failed connection.")
+
+    sys.exit()
+
+print()
+
+cursor = connection.cursor()
+
+
+
     def __init__(self):
         self.databaseName = databaseName
         self.tableName = tableName
@@ -9,3 +29,39 @@ class inv():
 
     def inventory(self):
         
+    def viewInventory(self):
+
+        cursor.execute("SELECT * FROM books")
+
+        
+
+    def searchInventory(self):
+
+        print("Enter the title of the book: ")
+        
+        cursor.execute("SELECT * FROM books")
+
+        result = cursor.fetchall()
+
+        print("Entire result set: ", result, sep="\n", end="\n\n\n")
+
+        for x in result:
+
+            print("Entire row:", x, "\n") ## all
+
+            print("Row broken down into each column: ")
+            
+            for y in x:
+                print(y)
+            
+            print()
+
+            print("ISBN:", x[0]) ## only the ISBN
+            print("Title:", x[1], "\tAuthor:", x[2])
+            print("\n\n")
+
+
+    def decreaseStock(self, ISBN):
+
+    
+
