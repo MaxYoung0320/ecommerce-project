bf35d8ebe2e26893f1194b537acfa8b0c4c35501
Johnnybro11 <landonjcessna@gmail.com>
2023-11-28 10:36:34 -0600

    trying to fix big oopsie

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from user import User
 print("Hello World")
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 viewInv = inv("", "")
 viewInv.viewInventory()
 isbn = input("Enter the isbn to delete: ")
 viewInv.decreaseStock(isbn)
 =======
+=======
+>>>>>>> ba2554bc88b407bded2c871b86ff64b2913c94df
 # viewInv = inv("", "")
 
 # viewInv.viewInventory()
@@ -17,5 +20,9 @@ viewInv.decreaseStock(isbn)
 user = User("db.db", "User")
 user.createAccount()
 user.login()
+<<<<<<< HEAD
+user.viewAccountInformation()
+>>>>>>> ba2554bc88b407bded2c871b86ff64b2913c94df
+=======
 user.viewAccountInformation()
 >>>>>>> ba2554bc88b407bded2c871b86ff64b2913c94df
