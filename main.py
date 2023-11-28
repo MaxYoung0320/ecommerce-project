fbd9c86b94a3ddd07f249d19677dd80846e512f7
Johnnybro11 <landonjcessna@gmail.com>
2023-11-28 10:32:18 -0600

    updated inv

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from inv import inv
+from user import User
 
 print("Hello World")
 
+<<<<<<< HEAD
 viewInv = inv("", "")
 viewInv.viewInventory()
 isbn = input("Enter the isbn to delete: ")
 viewInv.decreaseStock(isbn)
+=======
+# viewInv = inv("", "")
+
+# viewInv.viewInventory()
+
+
+user = User("db.db", "User")
+user.createAccount()
+user.login()
+user.viewAccountInformation()
+>>>>>>> ba2554bc88b407bded2c871b86ff64b2913c94df
